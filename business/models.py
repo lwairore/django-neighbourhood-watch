def98caa22fe5cf862f22b596fe2de9e10e0cb2e
lwairore <kwairore@gmail.com>
2019-07-06 10:05:54 +0300

    Change the ForeignKey of neighbourhood_id from Profile to Neighbourhood.

--- business/models.py
+++ business/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from profile_user.models import Profile
+from profile_user.models import Profile, Neighbourhood
 
 # Create your models here.
 class Business(models.Model):
     name = models.CharField(max_length=30)
     user_id = models.ForeignKey(User)
     image = models.ImageField(upload_to='business/')
-    neighbourhood_id = models.ForeignKey(Profile)
+    neighbourhood_id = models.ForeignKey(Neighbourhood)
     email = models.EmailField()
     
