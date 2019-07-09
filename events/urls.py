e7ca3743b3c28c633bc0a1d3bfe9e320f4715dfa
lwairore <kwairore@gmail.com>
2019-07-09 19:45:49 +0300

    Import url from django.conf.urls, views module, settings from django.conf, static from django.conf.urls.static, create urlpatterns that has event url and media url

---
+++ events/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.event, name='event' )
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
