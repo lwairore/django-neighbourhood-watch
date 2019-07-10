f0034a6e877b121a00a615e985aa413035e99bc8
lwairore <kwairore@gmail.com>
2019-07-10 08:03:57 +0300

    Register Contact, Events, Business models to admin.

--- profile_user/admin.py
+++ profile_user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Neighbourhood
+from .models import Neighbourhood, Profile
 
 # Register your models here.
 admin.site.register(Neighbourhood)
+admin.site.register(Profile)
