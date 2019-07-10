51fa73bcb7a6e4b9b585e8e199cf410bbf5c6cf1
lwairore <kwairore@gmail.com>
2019-07-10 08:30:53 +0300

    Remove .vscode

--- function-scope.js
+++
@@ -1,5 +0,0 @@
-var name = 'James';
-var changeName = function () { return name2 = 'Jonah'; };
-console.log('initial name:', changeName());
-name2 = 'Ben';
-changeName();
--- function-scope.ts
+++
@@ -1,5 +0,0 @@
-var name = 'James'
-var changeName = () => name2 = 'Jonah'
-console.log('initial name:', changeName())
-name2 = 'Ben'
-changeName()
